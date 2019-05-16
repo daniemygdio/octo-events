0d1a0240467009dd1b1692ccd360fb0df42a09c2
Danielle Emygdio <emygdio.d@gmail.com>
2019-05-16 15:43:00 -0300

    Add request test developed so far (not working)

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe "Events", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /events" do
+    context "when valid parameters" do
+      github_action = %w[opened closed].sample
+      
+      it "creates successfully" do
+        headers = { "CONTENT_TYPE" => "application/json" }
+
+        post "/events", :params => '{ action: github_action, issue: { url: "fake.url", number: 123 } }', :headers => headers
+
+        expect(response).to have_http_status(:created)
+      end
+    end
+  end
 end
